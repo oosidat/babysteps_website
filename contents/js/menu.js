216479ada113e66803cdca641ebc159cfd3ab3a8
Osama Sidat <oosidat@gmail.com>
2015-02-20 22:20:23 -0500

    removed jquery, vanilla js menu now

--- contents/js/menu.js
+++ contents/js/menu.js
@@ -1,6 +1,16 @@
-$(function() {
-    $("#menuExpand").click(function(e) {
-        $(".menu").toggleClass("menuOpen");
-        e.preventDefault();
-    });
+document.getElementById("menuExpand").addEventListener("click", function(event){
+    event.preventDefault()
+    toggleMenu();
 });
+
+function toggleMenu() {
+  var classToToggle = "menuOpen";
+  var menu = document.getElementsByClassName("menu")[0]
+  var currentClass = menu.className;
+  if (currentClass.indexOf(classToToggle) > -1) {
+    replaced = currentClass.replace( /(?:^|\s)menuOpen(?!\S)/g , '' );
+    menu.className = replaced;
+  } else {
+    menu.className += " menuOpen"
+  }
+}
